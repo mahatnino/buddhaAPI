fadfd5b7517c5e3d263745a74e8941e182aef6a4
Mahat <subash.mahat@cotiviti.com>
2022-11-26 00:08:36 +0545

    tesseract project added

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ import urllib.request
 from werkzeug.utils import secure_filename
 from flask_cors import CORS
 from pathlib import Path
+import pytesseract 
+import cv2
+from PIL import Image
+from pytesseract import Output
+import pandas as pd
+import numpy as np
+import json
  
 app = Flask(__name__)
 CORS(app)
@@ -20,6 +27,30 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def image_conversion(img):
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Users\subash.mahat\AppData\Local\Tesseract-OCR\tesseract.exe'
+    img_file = img
+    file_name = img_file.split('/')[2]
+    img = cv2.imread(img_file)
+    #gray image 
+    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Converting grey image to binary image by Thresholding
+    thresh_img = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+    # configuring parameters for tesseract
+    custom_config = r'--oem 3 --psm 6'
+ 
+    # Get all OCR output information from pytesseract
+    ocr_output_details = pytesseract.image_to_data(thresh_img,output_type=Output.DICT, config=custom_config, lang='eng')
+    df = pd.DataFrame.from_dict(ocr_output_details)
+    df['text'].replace('', np.nan, inplace = True)
+    df.dropna(subset=['text'], inplace = True)
+    for i in df.index:
+        (x, y, w, h) = (df['left'][i], df['top'][i], df['width'][i], df['height'][i])
+        img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 0), 10)
+    cv2.imwrite('static/uploads/'+file_name,img)
+       
 @app.route("/")
 def main():
     return 'Homepage'
@@ -50,12 +81,16 @@ def upload_file():
         resp.status_code = 500
         return resp
     if success:
-        resp = jsonify({'message' : 'Files successfully uploaded : '+ filename  }  )
+        image_conversion('static/uploads/' + filename)
+        resp = jsonify( {'message' :'Files successfully uploaded : '+ filename  }  )
         resp.status_code = 201
-        return resp
+        #json.dumps(True)
+        #app.response_class(json.dumps(True), content_type='application/json')
+        return jsonify({'message' : 'Files successfully uploaded', 'ok': bool(1) })
     else:
         resp = jsonify(errors)
-        resp.status_
+        #resp.status_
+        return jsonify({'message' : 'Files failed to uploaded', 'ok': bool(0) })
 
 
 @app.route('/uploadReturn/<filename>', methods=['GET'])
