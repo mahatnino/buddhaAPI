8fedec95dee20baa6827988b592b67ab440f05a1
Mahat <subash.mahat@cotiviti.com>
2022-11-25 16:16:30 +0545

    api post and get

--- app.py
+++ app.py
@@ -1,8 +1,66 @@
 #app.py
-from flask import Flask 
-
+from flask import Flask, json, request, jsonify
+import os
+import urllib.request
+from werkzeug.utils import secure_filename
+ 
 app = Flask(__name__)
+ 
+app.secret_key = "caircocoders-ednalan"
+ 
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+ 
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/")
-def index():
-    return "Hello World!!!!"
+def main():
+    return 'Homepage'
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # check if the post request has the file part
+    if 'files[]' not in request.files:
+        resp = jsonify({'message' : 'No file part in the request'})
+        resp.status
+
+    files = request.files.getlist('files[]')
+     
+    errors = {}
+    success = False
+     
+    for file in files:      
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            success = True
+        else:
+            errors[file.filename] = 'File type is not allowed'
+    
+    if success and errors:
+        errors['message'] = 'File(s) successfully uploaded'
+        resp = jsonify(errors)
+        resp.status_code = 500
+        return resp
+    if success:
+        resp = jsonify({'message' : 'Files successfully uploaded : '+ filename  }  )
+        resp.status_code = 201
+        return resp
+    else:
+        resp = jsonify(errors)
+        resp.status_
+
+@app.route('/uploadReturn', methods=['GET'])
+def uploadReturn_file():
+    args = request.args
+    returnFilename = args.get('fname')
+    resp = jsonify({'filename' : + returnFilename})
+    resp.status_code = 201
+    return resp
+
+if __name__ == '__main__':
+    app.run(debug=True)
